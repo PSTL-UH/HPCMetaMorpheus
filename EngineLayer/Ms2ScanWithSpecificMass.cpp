a5db9b3273ae37c15fe152aa6549990dde0956c3
Edgar Gabriel <egabriel@central.uh.edu>
2019-08-27 19:10:23 -0500

    EngineLayer/Ms2ScanWithSpecificMasses: make file compile
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

--- EngineLayer/Ms2ScanWithSpecificMass.cpp
+++ EngineLayer/Ms2ScanWithSpecificMass.cpp
@@ -1,5 +1,10 @@
-﻿#include "Ms2ScanWithSpecificMass.h"
+﻿#include <tuple>
+#include <algorithm>
+
+#include "Ms2ScanWithSpecificMass.h"
 #include "CommonParameters.h"
+
+#include "BinarySearch.h"
 
 using namespace Chemistry;
 using namespace MassSpectrometry;
@@ -9,155 +14,179 @@ namespace EngineLayer
 
 	Ms2ScanWithSpecificMass::Ms2ScanWithSpecificMass(MsDataScan *mzLibScan, double precursorMonoisotopicPeakMz, int precursorCharge, const std::string &fullFilePath, CommonParameters *commonParam, std::vector<IsotopicEnvelope*> &neutralExperimentalFragments)
 	{
-		PrecursorMonoisotopicPeakMz = precursorMonoisotopicPeakMz;
-		PrecursorCharge = precursorCharge;
-		PrecursorMass = getPrecursorMonoisotopicPeakMz().ToMass(precursorCharge);
-		FullFilePath = fullFilePath;
+            privatePrecursorMonoisotopicPeakMz = precursorMonoisotopicPeakMz;
+            privatePrecursorCharge = precursorCharge;
+            privatePrecursorMass = Chemistry::ClassExtensions::ToMass(getPrecursorMonoisotopicPeakMz(), precursorCharge);
+            privateFullFilePath = fullFilePath;
 
-		TheScan = mzLibScan;
+            privateTheScan = mzLibScan;
 
-		setExperimentalFragments(neutralExperimentalFragments ? neutralExperimentalFragments : GetNeutralExperimentalFragments(mzLibScan, commonParam));
-
-		if (getExperimentalFragments().Any())
-		{
-			DeconvolutedMonoisotopicMasses = getExperimentalFragments().Select([&] (std::any p)
-			{
-				p::monoisotopicMass;
-			})->ToArray();
-		}
+            setExperimentalFragments(neutralExperimentalFragments.empty() ? neutralExperimentalFragments : GetNeutralExperimentalFragments(mzLibScan, commonParam));
+            
+#ifdef ORIG
+            if (getExperimentalFragments().Any()) {
+                DeconvolutedMonoisotopicMasses = getExperimentalFragments().Select([&] (std::any p) {
+                        p::monoisotopicMass;
+                    })->ToArray();
+            }
+#endif
+            if (!getExperimentalFragments().empty()) {
+                for ( auto p: getExperimentalFragments() ) {
+                    DeconvolutedMonoisotopicMasses.push_back(p->monoisotopicMass );
+                }
+            }
 	}
 
-	MsDataScan *Ms2ScanWithSpecificMass::getTheScan() const
-	{
-		return privateTheScan;
-	}
+    
+    MsDataScan *Ms2ScanWithSpecificMass::getTheScan() const
+    {
+        return privateTheScan;
+    }
+    
+    double Ms2ScanWithSpecificMass::getPrecursorMonoisotopicPeakMz() const
+    {
+        return privatePrecursorMonoisotopicPeakMz;
+    }
+    
+    double Ms2ScanWithSpecificMass::getPrecursorMass() const
+    {
+        return privatePrecursorMass;
+    }
+    
+    int Ms2ScanWithSpecificMass::getPrecursorCharge() const
+    {
+        return privatePrecursorCharge;
+    }
+    
+    std::string Ms2ScanWithSpecificMass::getFullFilePath() const
+    {
+        return privateFullFilePath;
+    }
+    
+    std::vector<IsotopicEnvelope*> Ms2ScanWithSpecificMass::getExperimentalFragments() const
+    {
+        return privateExperimentalFragments;
+    }
+    
+    void Ms2ScanWithSpecificMass::setExperimentalFragments(const std::vector<IsotopicEnvelope*> &value)
+    {
+        privateExperimentalFragments = value;
+    }
+    
+    int Ms2ScanWithSpecificMass::getOneBasedScanNumber() const
+    {
+        return getTheScan()->getOneBasedScanNumber();
+    }
+    
+    std::optional<int> Ms2ScanWithSpecificMass::getOneBasedPrecursorScanNumber() const
+    {
+        return getTheScan()->getOneBasedPrecursorScanNumber();
+    }
+    
+    double Ms2ScanWithSpecificMass::getRetentionTime() const
+    {
+        return getTheScan()->getRetentionTime();
+    }
+    
+    int Ms2ScanWithSpecificMass::getNumPeaks() const
+    {
+        return getTheScan()->getMassSpectrum()->getSize();
+    }
+    
+    double Ms2ScanWithSpecificMass::getTotalIonCurrent() const
+    {
+        return getTheScan()->getTotalIonCurrent();
+    }
+    
+    std::vector<IsotopicEnvelope*> Ms2ScanWithSpecificMass::GetNeutralExperimentalFragments(MsDataScan *scan, CommonParameters *commonParam)
+    {
+        int minZ = 1;
+        int maxZ = 10;
+        
+        auto neutralExperimentalFragmentMasses = scan->getMassSpectrum()->Deconvolute(scan->getMassSpectrum()->getRange(),
+                                                              minZ, maxZ,
+                                                              commonParam->getDeconvolutionMassTolerance()->getValue(),
+                                                              commonParam->getDeconvolutionIntensityRatio());
+        
+        if (commonParam->getAssumeOrphanPeaksAreZ1Fragments())
+        {
+#ifdef ORIG
+            std::unordered_set<double> alreadyClaimedMzs = std::unordered_set<double>(neutralExperimentalFragmentMasses.SelectMany([&] (std::any p)
+            {
+                p::peaks->Select([&] (std::any v)
+            {
+                Chemistry::ClassExtensions::RoundedDouble(v::mz)->value();
+            });
+            }));
+#endif
+            std::unordered_set<double> alreadyClaimedMzs;
+            for ( IsotopicEnvelope* p: neutralExperimentalFragmentMasses ) {
+                for ( auto v: p->peaks ) {
+                    alreadyClaimedMzs.insert(Chemistry::ClassExtensions::RoundedDouble(std::get<0>(v)) );
+                }
+            }
+            
+            for (int i = 0; i < (int) scan->getMassSpectrum()->getXArray().size(); i++)
+            {
+                double mz = scan->getMassSpectrum()->getXArray()[i];
+                double intensity = scan->getMassSpectrum()->getYArray()[i];
+                
+                if ( std::find(alreadyClaimedMzs.begin(), alreadyClaimedMzs.end(), Chemistry::ClassExtensions::RoundedDouble(mz)) == alreadyClaimedMzs.end() )
+                {
+                    std::vector<std::tuple<double, double>> dt = {std::make_tuple(mz, intensity)};
+                    IsotopicEnvelope tempVar(dt, Chemistry::ClassExtensions::ToMass(mz, 1), 1, intensity, 0, 0);
+                    neutralExperimentalFragmentMasses.push_back(&tempVar);
+                }
+            }
+    
+        }
+#ifdef ORIG
+        return neutralExperimentalFragmentMasses.OrderBy([&] (std::any p)  {
+                p::monoisotopicMass;
+            })->ToArray();
+#endif
+        std::sort ( neutralExperimentalFragmentMasses.begin(), neutralExperimentalFragmentMasses.end(), [&] (IsotopicEnvelope *e1, IsotopicEnvelope *e2 ) {
+                return e1->monoisotopicMass > e2->monoisotopicMass;
+            });
+        return neutralExperimentalFragmentMasses;
+    }
 
-	double Ms2ScanWithSpecificMass::getPrecursorMonoisotopicPeakMz() const
-	{
-		return privatePrecursorMonoisotopicPeakMz;
-	}
+    IsotopicEnvelope *Ms2ScanWithSpecificMass::GetClosestExperimentalFragmentMass(double theoreticalNeutralMass)
+    {
+        if (DeconvolutedMonoisotopicMasses.empty())
+        {
+            return nullptr;
+        }
+        return getExperimentalFragments()[GetClosestFragmentMass(theoreticalNeutralMass).value()];
+    }
 
-	double Ms2ScanWithSpecificMass::getPrecursorMass() const
-	{
-		return privatePrecursorMass;
-	}
-
-	int Ms2ScanWithSpecificMass::getPrecursorCharge() const
-	{
-		return privatePrecursorCharge;
-	}
-
-	std::string Ms2ScanWithSpecificMass::getFullFilePath() const
-	{
-		return privateFullFilePath;
-	}
-
-	std::vector<IsotopicEnvelope*> Ms2ScanWithSpecificMass::getExperimentalFragments() const
-	{
-		return privateExperimentalFragments;
-	}
-
-	void Ms2ScanWithSpecificMass::setExperimentalFragments(const std::vector<IsotopicEnvelope*> &value)
-	{
-		privateExperimentalFragments = value;
-	}
-
-	int Ms2ScanWithSpecificMass::getOneBasedScanNumber() const
-	{
-		return getTheScan()->OneBasedScanNumber;
-	}
-
-	std::optional<int> Ms2ScanWithSpecificMass::getOneBasedPrecursorScanNumber() const
-	{
-		return getTheScan()->OneBasedPrecursorScanNumber;
-	}
-
-	double Ms2ScanWithSpecificMass::getRetentionTime() const
-	{
-		return getTheScan()->RetentionTime;
-	}
-
-	int Ms2ScanWithSpecificMass::getNumPeaks() const
-	{
-		return getTheScan()->MassSpectrum.Size;
-	}
-
-	double Ms2ScanWithSpecificMass::getTotalIonCurrent() const
-	{
-		return getTheScan()->TotalIonCurrent;
-	}
-
-	std::vector<IsotopicEnvelope*> Ms2ScanWithSpecificMass::GetNeutralExperimentalFragments(MsDataScan *scan, CommonParameters *commonParam)
-	{
-		int minZ = 1;
-		int maxZ = 10;
-
-		auto neutralExperimentalFragmentMasses = scan->MassSpectrum.Deconvolute(scan->MassSpectrum.Range, minZ, maxZ, commonParam->getDeconvolutionMassTolerance()->Value, commonParam->getDeconvolutionIntensityRatio()).ToList();
-
-		if (commonParam->getAssumeOrphanPeaksAreZ1Fragments())
-		{
-			std::unordered_set<double> alreadyClaimedMzs = std::unordered_set<double>(neutralExperimentalFragmentMasses.SelectMany([&] (std::any p)
-			{
-				p::peaks->Select([&] (std::any v)
-				{
-					ClassExtensions::RoundedDouble(v::mz)->Value;
-				});
-			}));
-
-			for (int i = 0; i < scan->MassSpectrum.XArray->Length; i++)
-			{
-				double mz = scan->MassSpectrum.XArray[i];
-				double intensity = scan->MassSpectrum.YArray[i];
-
-				if (!std::find(alreadyClaimedMzs.begin(), alreadyClaimedMzs.end(), ClassExtensions::RoundedDouble(mz)->Value) != alreadyClaimedMzs.end()->Value))
-				{
-					IsotopicEnvelope tempVar({(mz, intensity)}, mz.ToMass(1), 1, intensity, 0, 0);
-					neutralExperimentalFragmentMasses.push_back(&tempVar);
-				}
-			}
-		}
-
-		return neutralExperimentalFragmentMasses.OrderBy([&] (std::any p)
-		{
-			p::monoisotopicMass;
-		})->ToArray();
-	}
-
-	IsotopicEnvelope *Ms2ScanWithSpecificMass::GetClosestExperimentalFragmentMass(double theoreticalNeutralMass)
-	{
-		if (DeconvolutedMonoisotopicMasses.empty())
-		{
-			return nullptr;
-		}
-		return getExperimentalFragments()[GetClosestFragmentMass(theoreticalNeutralMass).Value];
-	}
-
-	std::optional<int> Ms2ScanWithSpecificMass::GetClosestFragmentMass(double mass)
-	{
-		if (DeconvolutedMonoisotopicMasses.empty())
-		{
-			return std::nullopt;
-		}
-		int index = Array::BinarySearch(DeconvolutedMonoisotopicMasses, mass);
-		if (index >= 0)
-		{
-			return std::make_optional(index);
-		}
-		index = ~index;
-
-		if (index >= DeconvolutedMonoisotopicMasses.size())
-		{
-			return std::make_optional(index - 1);
-		}
-		if (index == 0)
-		{
-			return std::make_optional(index);
-		}
-
-		if (mass - DeconvolutedMonoisotopicMasses[index - 1] > DeconvolutedMonoisotopicMasses[index] - mass)
-		{
-			return std::make_optional(index);
-		}
-		return std::make_optional(index - 1);
-	}
+    std::optional<int> Ms2ScanWithSpecificMass::GetClosestFragmentMass(double mass)
+    {
+        if (DeconvolutedMonoisotopicMasses.empty())
+        {
+            return std::nullopt;
+        }
+        //int index = Array::BinarySearch(DeconvolutedMonoisotopicMasses, mass);
+        int index = binarySearch(DeconvolutedMonoisotopicMasses, mass);
+        if (index >= 0)
+        {
+            return std::make_optional(index);
+        }
+        index = ~index;
+        
+        if (index >= (int)DeconvolutedMonoisotopicMasses.size())
+        {
+            return std::make_optional(index - 1);
+        }
+        if (index == 0)
+        {
+            return std::make_optional(index);
+        }
+        
+        if (mass - DeconvolutedMonoisotopicMasses[index - 1] > DeconvolutedMonoisotopicMasses[index] - mass)
+        {
+            return std::make_optional(index);
+        }
+        return std::make_optional(index - 1);
+    }
 }
--- EngineLayer/Ms2ScanWithSpecificMass.h
+++ EngineLayer/Ms2ScanWithSpecificMass.h
@@ -8,52 +8,56 @@
 #include <optional>
 
 //C# TO C++ CONVERTER NOTE: Forward class declarations:
-namespace EngineLayer { class CommonParameters; }
+//namespace EngineLayer { class CommonParameters; }
+#include "CommonParameters.h"
 
+#include "Chemistry/Chemistry.h"
 using namespace Chemistry;
+
+#include "MassSpectrometry/MassSpectrometry.h"
 using namespace MassSpectrometry;
 
 namespace EngineLayer
 {
-	class Ms2ScanWithSpecificMass : public IScan
-	{
-	private:
-		MsDataScan *privateTheScan;
-		double privatePrecursorMonoisotopicPeakMz = 0;
-		double privatePrecursorMass = 0;
-		int privatePrecursorCharge = 0;
-		std::string privateFullFilePath;
-		std::vector<IsotopicEnvelope*> privateExperimentalFragments;
-
-	public:
-		Ms2ScanWithSpecificMass(MsDataScan *mzLibScan, double precursorMonoisotopicPeakMz, int precursorCharge, const std::string &fullFilePath, CommonParameters *commonParam, std::vector<IsotopicEnvelope*> &neutralExperimentalFragments);
-
-		MsDataScan *getTheScan() const;
-		double getPrecursorMonoisotopicPeakMz() const override;
-		double getPrecursorMass() const override;
-		int getPrecursorCharge() const override;
-		std::string getFullFilePath() const override;
-		std::vector<IsotopicEnvelope*> getExperimentalFragments() const;
-		void setExperimentalFragments(const std::vector<IsotopicEnvelope*> &value);
-	private:
-		std::vector<double> DeconvolutedMonoisotopicMasses;
-
-	public:
-		int getOneBasedScanNumber() const override;
-
-		std::optional<int> getOneBasedPrecursorScanNumber() const override;
-
-		double getRetentionTime() const override;
-
-		int getNumPeaks() const override;
-
-		double getTotalIonCurrent() const override;
-
-		static std::vector<IsotopicEnvelope*> GetNeutralExperimentalFragments(MsDataScan *scan, CommonParameters *commonParam);
-
-		IsotopicEnvelope *GetClosestExperimentalFragmentMass(double theoreticalNeutralMass);
-
-	private:
-		std::optional<int> GetClosestFragmentMass(double mass);
-	};
+    class Ms2ScanWithSpecificMass : public IScan
+    {
+    private:
+        MsDataScan *privateTheScan;
+        double privatePrecursorMonoisotopicPeakMz = 0;
+        double privatePrecursorMass = 0;
+        int privatePrecursorCharge = 0;
+        std::string privateFullFilePath;
+        std::vector<IsotopicEnvelope*> privateExperimentalFragments;
+        
+    public:
+        Ms2ScanWithSpecificMass(MsDataScan *mzLibScan, double precursorMonoisotopicPeakMz, int precursorCharge, const std::string &fullFilePath, CommonParameters *commonParam, std::vector<IsotopicEnvelope*> &neutralExperimentalFragments);
+        
+        MsDataScan *getTheScan() const;
+        double getPrecursorMonoisotopicPeakMz() const override;
+        double getPrecursorMass() const override;
+        int getPrecursorCharge() const override;
+        std::string getFullFilePath() const override;
+        std::vector<IsotopicEnvelope*> getExperimentalFragments() const;
+        void setExperimentalFragments(const std::vector<IsotopicEnvelope*> &value);
+    private:
+        std::vector<double> DeconvolutedMonoisotopicMasses;
+        
+    public:
+        int getOneBasedScanNumber() const override;
+        
+        std::optional<int> getOneBasedPrecursorScanNumber() const override;
+        
+        double getRetentionTime() const override;
+        
+        int getNumPeaks() const override;
+        
+        double getTotalIonCurrent() const override;
+        
+        static std::vector<IsotopicEnvelope*> GetNeutralExperimentalFragments(MsDataScan *scan, CommonParameters *commonParam);
+        
+        IsotopicEnvelope *GetClosestExperimentalFragmentMass(double theoreticalNeutralMass);
+        
+    private:
+        std::optional<int> GetClosestFragmentMass(double mass);
+    };
 }
