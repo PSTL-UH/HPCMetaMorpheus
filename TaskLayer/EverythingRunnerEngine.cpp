22fd914d52155b7ac396febe3dc2e46a591169ce
Edgar Gabriel <egabriel@central.uh.edu>
2020-04-20 15:14:25 -0500

    EverythingRunnerEngine.cpp: file compiles now.
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

--- TaskLayer/EverythingRunnerEngine.cpp
+++ TaskLayer/EverythingRunnerEngine.cpp
@@ -6,13 +6,17 @@
 #include "../EngineLayer/EventArgs/StringListEventArgs.h"
 #include "EventHandler.h"
 #include "EventArgs/XmlForTaskListEventArgs.h"
+#include "CalibrationTask/CalibrationTask.h"
+
+#include <experimental/filesystem>
+#include <ctime>
 
 using namespace EngineLayer;
 
 namespace TaskLayer
 {
 
-    EverythingRunnerEngine::EverythingRunnerEngine(std::vector<std::tuple<std::string, MetaMorpheusTask>*> &taskList,
+    EverythingRunnerEngine::EverythingRunnerEngine(std::vector<std::tuple<std::string, MetaMorpheusTask*>> &taskList,
                                                    std::vector<std::string> &startingRawFilenameList,
                                                    std::vector<DbForTask*> &startingXmlDbFilenameList,
                                                    const std::string &outputFolder) : TaskList(taskList)
@@ -22,19 +26,24 @@ namespace TaskLayer
         CurrentRawDataFilenameList = startingRawFilenameList;
         CurrentXmlDbFilenameList = startingXmlDbFilenameList;
         
-        StartingAllTasksEngineHandler = new EventHandler();
+        StartingAllTasksEngineHandler = new EventHandler<StringEventArgs>();
         FinishedAllTasksEngineHandler = new EventHandler<StringEventArgs>();
         NewDbsHandler = new EventHandler<XmlForTaskListEventArgs>();
         NewSpectrasHandler = new EventHandler<StringListEventArgs>();
         NewFileSpecificTomlHandler = new EventHandler<StringListEventArgs>();
         WarnHandler = new EventHandler<StringEventArgs>();
         FinishedWritingAllResultsFileHandler = new EventHandler<StringEventArgs>();
-        
     }
     
     void EverythingRunnerEngine::Run()
     {
+
         StartingAllTasks();
+        time_t timer;
+        time(&timer);
+        struct tm *tmi_start = localtime(&timer);
+        clock_t begin = clock();
+        
         if (!CurrentRawDataFilenameList.empty())
         {
             Warn("No spectra files selected");
@@ -43,7 +52,11 @@ namespace TaskLayer
             return;
         }
         
-        auto startTimeForAllFilenames = DateTime::Now.ToString("yyyy-MM-dd-HH-mm-ss", CultureInfo::InvariantCulture);
+        //auto startTimeForAllFilenames = DateTime::Now.ToString("yyyy-MM-dd-HH-mm-ss", CultureInfo::InvariantCulture);
+        std::string startTimeForAllFilenames = std::to_string(tmi_start->tm_year) + "-" +
+            std::to_string(tmi_start->tm_mon) + "-" +  std::to_string(tmi_start->tm_mday) + "-" +
+            std::to_string(tmi_start->tm_hour) + "-" + std::to_string(tmi_start->tm_min) + "-" +
+            std::to_string(tmi_start->tm_sec);
         
         OutputFolder = StringHelper::replace(OutputFolder, "$DATETIME", startTimeForAllFilenames);
         
@@ -69,59 +82,108 @@ namespace TaskLayer
             }
             auto ok = TaskList[i];
             
-            auto outputFolderForThisTask = FileSystem::combine(OutputFolder, ok->Item1);
+            auto outputFolderForThisTask = OutputFolder + std::get<0>(ok);
             
-            if (!FileSystem::directoryExists(outputFolderForThisTask))
+            if (!std::experimental::filesystem::exists(outputFolderForThisTask))
             {
-                FileSystem::createDirectory(outputFolderForThisTask);
+                std::experimental::filesystem::create_directory(outputFolderForThisTask);
             }
             
             // Actual task running code
-            auto myTaskResults = ok->Item2->RunTask(outputFolderForThisTask, CurrentXmlDbFilenameList, CurrentRawDataFilenameList, ok->Item1);
+            auto myTaskResults = std::get<1>(ok)->RunTask(outputFolderForThisTask,
+                                                          CurrentXmlDbFilenameList,
+                                                          CurrentRawDataFilenameList,
+                                                          std::get<0>(ok));
             
-            if (myTaskResults->NewDatabases != nullptr)
+            if (!myTaskResults->NewDatabases.empty() )
             {
                 CurrentXmlDbFilenameList = myTaskResults->NewDatabases;
                 NewDBs(myTaskResults->NewDatabases);
             }
-            if (myTaskResults->NewSpectra != nullptr)
+            if (!myTaskResults->NewSpectra.empty())
             {
-                if (CurrentRawDataFilenameList.size() == myTaskResults->NewSpectra->Count)
+                if (CurrentRawDataFilenameList.size() == myTaskResults->NewSpectra.size() )
                 {
                     CurrentRawDataFilenameList = myTaskResults->NewSpectra;
                 }
                 else
                 {
                     // at least one file was not successfully calibrated
+#ifdef ORIG
                     auto successfullyCalibFiles = myTaskResults->NewSpectra->Select([&] (std::any p)  {
                             StringHelper::replace(Path::GetFileNameWithoutExtension(p), CalibrationTask::CalibSuffix, "");
                         }).ToList();
+#endif
+                    std::vector<std::string> successfullyCalibFiles;
+                    for (auto p: myTaskResults->NewSpectra ) {
+                        std::string fname = p.substr(0, p.find_last_of("."));
+                        successfullyCalibFiles.push_back( StringHelper::replace(fname, CalibrationTask::CalibSuffix, ""));
+                    }
+
+#ifdef ORIG
                     auto origFiles = CurrentRawDataFilenameList.Select([&] (std::any p) {
                             Path::GetFileNameWithoutExtension(p);
                         }).ToList();
+#endif
+                    std::vector<std::string> origFiles;
+                    for ( auto p: CurrentRawDataFilenameList ) {
+                        origFiles.push_back(p.substr(0,p.find_last_of(".")));
+                    }
+#ifdef ORIG
                     auto unsuccessfullyCalibFiles = origFiles.Except(successfullyCalibFiles).ToList();
+#endif
+                    std::vector<std::string> unsuccessfullyCalibFiles;
+                    for ( auto p: origFiles ) {
+                        bool found = false;
+                        for (auto q: successfullyCalibFiles ) {
+                            if ( q == p ) {
+                                found = true;
+                                break;
+                            }
+                        }
+                        if ( !found ) {
+                            unsuccessfullyCalibFiles.push_back(p);
+                        }
+                    }
+#ifdef ORIG
                     auto unsuccessfullyCalibFilePaths = CurrentRawDataFilenameList.Where([&] (std::any p)   {
-                            std::find(unsuccessfullyCalibFiles.begin(), unsuccessfullyCalibFiles.end(), Path::GetFileNameWithoutExtension(p)) != unsuccessfullyCalibFiles.end();
+                            std::find(unsuccessfullyCalibFiles.begin(), unsuccessfullyCalibFiles.end(),
+                                      Path::GetFileNameWithoutExtension(p)) != unsuccessfullyCalibFiles.end();
                         }).ToList();
+#endif
+                    std::vector<std::string> unsuccessfullyCalibFilePaths;
+                    for ( auto p: CurrentRawDataFilenameList ) {
+                        std::string p_dash = p.substr(0, p.find_last_of("."));
+                        if ( std::find(unsuccessfullyCalibFiles.begin(), unsuccessfullyCalibFiles.end(),
+                                       p_dash) != unsuccessfullyCalibFiles.end() ){
+                            unsuccessfullyCalibFilePaths.push_back(p);
+                        }
+                    }
+                    
                     CurrentRawDataFilenameList = myTaskResults->NewSpectra;
-                    CurrentRawDataFilenameList.insert(CurrentRawDataFilenameList.end(), unsuccessfullyCalibFilePaths.begin(), unsuccessfullyCalibFilePaths.end());
+
+                    CurrentRawDataFilenameList.insert(CurrentRawDataFilenameList.end(), unsuccessfullyCalibFilePaths.begin(),
+                                                      unsuccessfullyCalibFilePaths.end());
                 } 
                 
                 NewSpectras(myTaskResults->NewSpectra);
             }
-            if (myTaskResults->NewFileSpecificTomls != nullptr)
+            if (!myTaskResults->NewFileSpecificTomls.empty() )
             {
                 NewFileSpecificToml(myTaskResults->NewFileSpecificTomls);
             }
-            
-            allResultsText->appendLine("\r\n" + "\r\n" + "\r\n" + "\r\n" + myTaskResults->ToString());
+
+            std::string sst= "\r\n\r\n\r\n\r\n";
+            allResultsText->appendLine( sst + myTaskResults->ToString());
         }
+
+        clock_t end = clock();
         
-        auto resultsFileName = FileSystem::combine(OutputFolder, "allResults.txt");
-        StreamWriter file = StreamWriter(resultsFileName);
-        file.WriteLine("MetaMorpheus: version " + GlobalVariables::getMetaMorpheusVersion());
-        file.WriteLine("Total time: " + stopWatch->Elapsed);
-        file.Write(allResultsText->toString());
+        auto resultsFileName = OutputFolder + "/allResults.txt";
+        std::ofstream file(resultsFileName);
+        file << "MetaMorpheus: version " << GlobalVariables::getMetaMorpheusVersion() << std::endl;
+        file <<"Total time: " << (end - begin)/CLOCKS_PER_SEC << std::endl;
+        file << allResultsText->toString() << std::endl;
         
         std::vector<std::string> tmpvec;
         StringEventArgs tempVar(resultsFileName, tmpvec);
@@ -145,7 +207,7 @@ namespace TaskLayer
     void EverythingRunnerEngine::StartingAllTasks()
     {
         if ( StartingAllTasksEngineHandler != nullptr ) {
-            StartingAllTasksEngineHandler->Invoke(this, EventArgs::Empty);
+            StartingAllTasksEngineHandler->Invoke(this, nullptr);
         }
     }
     
--- TaskLayer/EverythingRunnerEngine.h
+++ TaskLayer/EverythingRunnerEngine.h
@@ -5,13 +5,12 @@
 #include <algorithm>
 #include "stringhelper.h"
 #include "stringbuilder.h"
-//#include "tangible_event.h"
 #include "tangible_filesystem.h"
 #include "EventHandler.h"
 #include "../EngineLayer/EventArgs/StringEventArgs.h"
 #include "../EngineLayer/EventArgs/StringListEventArgs.h"
+#include "EventArgs/XmlForTaskListEventArgs.h"
 
-//C# TO C++ CONVERTER NOTE: Forward class declarations:
 #include "MetaMorpheusTask.h"
 #include "DbForTask.h"
 
@@ -19,51 +18,51 @@ using namespace EngineLayer;
 
 namespace TaskLayer
 {
-	class EverythingRunnerEngine
-	{
-	private:
-		const std::vector<(std::string, MetaMorpheusTask)*> TaskList;
-		std::string OutputFolder;
-		std::vector<std::string> CurrentRawDataFilenameList;
-		std::vector<DbForTask*> CurrentXmlDbFilenameList;
-
-	public:
-		EverythingRunnerEngine(std::vector<std::tuple<std::string, MetaMorpheusTask>*> &taskList,
-                                       std::vector<std::string> &startingRawFilenameList,
-                                       std::vector<DbForTask*> &startingXmlDbFilenameList,
-                                       const std::string &outputFolder);
-
+    class EverythingRunnerEngine
+    {
+    private:
+        const std::vector<std::tuple<std::string, MetaMorpheusTask*>> TaskList;
+        std::string OutputFolder;
+        std::vector<std::string> CurrentRawDataFilenameList;
+        std::vector<DbForTask*> CurrentXmlDbFilenameList;
+        
+    public:
+        EverythingRunnerEngine(std::vector<std::tuple<std::string, MetaMorpheusTask*>> &taskList,
+                               std::vector<std::string> &startingRawFilenameList,
+                               std::vector<DbForTask*> &startingXmlDbFilenameList,
+                               const std::string &outputFolder);
+        
 #ifdef ORIG
-		static TangibleEvent<EventHandler<StringEventArgs>> *FinishedWritingAllResultsFileHandler = new TangibleEvent<EventHandler<StringEventArgs>>();
-		static TangibleEvent<EventHandler> *StartingAllTasksEngineHandler = new TangibleEvent<EventHandler>();
-		static TangibleEvent<EventHandler<StringEventArgs>> *FinishedAllTasksEngineHandler = new TangibleEvent<EventHandler<StringEventArgs>>();
-		static TangibleEvent<EventHandler<XmlForTaskListEventArgs>> *NewDbsHandler = new TangibleEvent<EventHandler<XmlForTaskListEventArgs>>();
-		static TangibleEvent<EventHandler<StringListEventArgs>> *NewSpectrasHandler = new TangibleEvent<EventHandler<StringListEventArgs>>();
-		static TangibleEvent<EventHandler<StringListEventArgs>> *NewFileSpecificTomlHandler = new TangibleEvent<EventHandler<StringListEventArgs>>();
-		static TangibleEvent<EventHandler<StringEventArgs>> *WarnHandler = new TangibleEvent<EventHandler<StringEventArgs>>();
-                static TangibleEvent<EventHandler<StringEventArgs>> *FinishedWritingAllResultsFileHandler = new TangibleEvent<EventHandler<StringEventArgs>>();
+        static TangibleEvent<EventHandler<StringEventArgs>> *FinishedWritingAllResultsFileHandler = new TangibleEvent<EventHandler<StringEventArgs>>();
+        static TangibleEvent<EventHandler> *StartingAllTasksEngineHandler = new TangibleEvent<EventHandler>();
+        static TangibleEvent<EventHandler<StringEventArgs>> *FinishedAllTasksEngineHandler = new TangibleEvent<EventHandler<StringEventArgs>>();
+        static TangibleEvent<EventHandler<XmlForTaskListEventArgs>> *NewDbsHandler = new TangibleEvent<EventHandler<XmlForTaskListEventArgs>>();
+        static TangibleEvent<EventHandler<StringListEventArgs>> *NewSpectrasHandler = new TangibleEvent<EventHandler<StringListEventArgs>>();
+        static TangibleEvent<EventHandler<StringListEventArgs>> *NewFileSpecificTomlHandler = new TangibleEvent<EventHandler<StringListEventArgs>>();
+        static TangibleEvent<EventHandler<StringEventArgs>> *WarnHandler = new TangibleEvent<EventHandler<StringEventArgs>>();
+        static TangibleEvent<EventHandler<StringEventArgs>> *FinishedWritingAllResultsFileHandler = new TangibleEvent<EventHandler<StringEventArgs>>();
 #endif
-		static EventHandler *StartingAllTasksEngineHandler;
-		static EventHandler<StringEventArgs> *FinishedAllTasksEngineHandler;
-		static EventHandler<XmlForTaskListEventArgs> *NewDbsHandler;
-		static EventHandler<StringListEventArgs> *NewSpectrasHandler;
-		static EventHandler<StringListEventArgs> *NewFileSpecificTomlHandler;
-		static EventHandler<StringEventArgs> *WarnHandler;
-                static EventHandler<StringEventArgs> *FinishedWritingAllResultsFileHandler;
-
-		void Run();
-
-	private:
-		void Warn(const std::string &v);
-
-		void StartingAllTasks();
-
-		void FinishedAllTasks(const std::string &rootOutputDir);
-
-		void NewSpectras(std::vector<std::string> &newSpectra);
-
-		void NewFileSpecificToml(std::vector<std::string> &newFileSpecificTomls);
-
-		void NewDBs(std::vector<DbForTask*> &newDatabases);
-	};
+        static EventHandler<StringEventArgs> *StartingAllTasksEngineHandler;
+        static EventHandler<StringEventArgs> *FinishedAllTasksEngineHandler;
+        static EventHandler<XmlForTaskListEventArgs> *NewDbsHandler;
+        static EventHandler<StringListEventArgs> *NewSpectrasHandler;
+        static EventHandler<StringListEventArgs> *NewFileSpecificTomlHandler;
+        static EventHandler<StringEventArgs> *WarnHandler;
+        static EventHandler<StringEventArgs> *FinishedWritingAllResultsFileHandler;
+            
+        void Run();
+        
+    private:
+        void Warn(const std::string &v);
+        
+        void StartingAllTasks();
+        
+        void FinishedAllTasks(const std::string &rootOutputDir);
+        
+        void NewSpectras(std::vector<std::string> &newSpectra);
+        
+        void NewFileSpecificToml(std::vector<std::string> &newFileSpecificTomls);
+        
+        void NewDBs(std::vector<DbForTask*> &newDatabases);
+    };
 }
