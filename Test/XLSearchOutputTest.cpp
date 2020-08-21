193fdbd24dd6f920a61a12bab55f939bf4486834
Edgar Gabriel <egabriel@central.uh.edu>
2020-08-21 12:41:58 -0500

    XLSearchOutputTest.cpp: make test compile and run
    the output is mostly correct, although it complains about 45 vs. 44 tab separated entries. Probably a miossing
    or miss-identifiued double tab that is not parsed correctly.
    
    The two values that differ from the C# version are the rank again (is 40 in the C# version but 68 in the C++ version),
    and the Beta Peptide Rank ( 28 in C#, 56 in C++ )
    
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

--- Test/XLSearchOutputTest.cpp
+++ Test/XLSearchOutputTest.cpp
@@ -1,30 +1,67 @@
 ﻿#include "XLSearchOutputTest.h"
-#include "../TaskLayer/XLSearchTask/TaskLayer.XLSearchTask.h"
+#include "../TaskLayer/XLSearchTask/XLSearchTask.h"
 #include "../TaskLayer/DbForTask.h"
 
-using namespace NUnit::Framework;
 using namespace TaskLayer;
+
+#include "Assert.h"
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+
+
+int main ( int argc, char **argv )
+{
+    int i=0;
+    std::cout << i << ". PeriodicTableLoader" << std::endl;
+    const std::string elfile="elements.dat";
+    const std::string &elr=elfile;
+    //Chemistry::PeriodicTable::Load (elr);
+    UsefulProteomicsDatabases::PeriodicTableLoader::Load (elr);
+
+    std::cout << ++i << ". WriteTsvTest" << std::endl;
+    Test::XLSearchOutputTest::WriteTsvTest();
+}
 
 namespace Test
 {
 
-	void XLSearchOutputTest::WriteTsvTest()
-	{
-		std::wstring outputFolder = FileSystem::combine(TestContext::CurrentContext->TestDirectory, LR"(XlOutputTest1)");
-		std::wstring myFile = FileSystem::combine(TestContext::CurrentContext->TestDirectory, LR"(XlTestData\BSA_DSS_23747.mzML)");
-		std::wstring myDatabase = FileSystem::combine(TestContext::CurrentContext->TestDirectory, LR"(XlTestData\BSA.fasta)");
-
-		FileSystem::createDirectory(outputFolder);
-
-		XLSearchTask *xLSearch = new XLSearchTask();
-		xLSearch->RunTask(outputFolder, {new DbForTask(myDatabase, false)}, {myFile}, L"test");
-
-		auto resultsPath = File::ReadAllLines(FileSystem::combine(outputFolder, LR"(XL_Interlinks.tsv)"));
-		auto sections = StringHelper::split(resultsPath[1], L'\t');
-		Assert::That(resultsPath.size() == 2);
-		Assert::That(sections.size() == 45);
-		Directory::Delete(outputFolder, true);
-
-		delete xLSearch;
-	}
+    void XLSearchOutputTest::WriteTsvTest()
+    {
+        std::string testdir=std::filesystem::current_path().string();
+        
+        std::string outputFolder = testdir + "/XlOutputTest1";
+        std::string myFile = testdir + "/XlTestData/BSA_DSS_23747.mzML";
+        std::string myDatabase = testdir + "/XlTestData/BSA.fasta";
+        
+        FileSystem::createDirectory(outputFolder);
+        
+        XLSearchTask *xLSearch = new XLSearchTask();
+        auto db = new DbForTask(myDatabase, false);
+        std::vector<DbForTask *> dbvec = {db};
+        std::vector<std::string> fvec = {myFile};
+        xLSearch->RunTask(outputFolder, dbvec, fvec, "test");
+        
+        //auto resultsPath = File::ReadAllLines(outputFolder + "/XL_Interlinks.tsv");
+        std::vector<std::string> resultsPath;
+        std::ifstream fs(outputFolder + "/XL_Interlinks.tsv" ) ;
+        if ( fs.is_open() ) {
+            std::string line;
+            while ( getline(fs, line ) ) {
+                resultsPath.push_back(line);
+                std::cout << line << std::endl;
+            }
+        }
+        else {
+            std::cout << "Could not open file " << outputFolder + "/XL_Interlinks.tsv" << std::endl;
+        }
+        if ( resultsPath.size() > 0 ) {
+            auto sections = StringHelper::split(resultsPath[1], '\t');
+            Assert::AreEqual((int)sections.size(), 45);
+        }
+        Assert::AreEqual((int)resultsPath.size(), 2);
+        std::filesystem::remove_all(outputFolder);
+        
+        delete xLSearch;
+    }
 }
--- Test/XLSearchOutputTest.h
+++ Test/XLSearchOutputTest.h
@@ -5,18 +5,12 @@
 #include "stringhelper.h"
 #include "tangible_filesystem.h"
 
-using namespace NUnit::Framework;
-using namespace TaskLayer;
 
 namespace Test
 {
-//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in native C++:
-//ORIGINAL LINE: [TestFixture] class XLSearchOutputTest
-	class XLSearchOutputTest
-	{
-	public:
-//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in native C++:
-//ORIGINAL LINE: [Test] public void WriteTsvTest()
-		void WriteTsvTest();
-	};
+    class XLSearchOutputTest
+    {
+    public:
+        static void WriteTsvTest();
+    };
 }
